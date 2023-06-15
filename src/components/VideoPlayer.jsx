b614afb81540f5f8ec7fc1627de6bbbc879cc89d
DonaldJasper0621 <donaldsu910621@gmail.com>
2023-06-15 16:49:14 +0800

    new deploy

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,25 +1,32 @@
-import { DefaultPlayer as Video } from 'react-html5video';
-import 'react-html5video/dist/styles.css'; 
+import { DefaultPlayer as Video } from "react-html5video";
+import "react-html5video/dist/styles.css";
 import TestVideo from "../assets/TestVideo.mp4";
-import "./VideoPlayer.css"
+import "./VideoPlayer.css";
 
-function VideoPlayer(){
-    return (
-      <div className='video-player-container '>
-        <Video 
-            className="video-player"  // Add this line
-            loop 
-            muted
-            controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}
-            poster="https://i.ytimg.com/vi/kMvPtA8GL9I/maxresdefault.jpg"
-            onCanPlayThrough={() => {
-                // Do stuff
-            }}>
-            <source src= {TestVideo} type="video/webm" />
-            <track label="English" kind="subtitles" srcLang="en" src="http://source.vtt" default />
-        </Video>
-        </div>
-    );
+function VideoPlayer() {
+  return (
+    <div className="video-player-container ">
+      <Video
+        className="video-player" // Add this line
+        loop
+        muted
+        controls={["PlayPause", "Seek", "Time", "Volume", "Fullscreen"]}
+        poster="https://i.ytimg.com/vi/kMvPtA8GL9I/maxresdefault.jpg"
+        onCanPlayThrough={() => {
+          // Do stuff
+        }}
+      >
+        <source src={TestVideo} type="video/webm" />
+        <track
+          label="English"
+          kind="subtitles"
+          srcLang="en"
+          src="http://source.vtt"
+          default
+        />
+      </Video>
+    </div>
+  );
 }
 
-export default VideoPlayer
+export default VideoPlayer;
