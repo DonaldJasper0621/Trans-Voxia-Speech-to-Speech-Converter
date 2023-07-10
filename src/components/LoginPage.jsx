c16ddbba813f8e61d18e730de549d4b2304bb2f1
DonaldJasper0621 <donaldsu910621@gmail.com>
2023-07-10 11:05:12 +0800

    four api

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -2,8 +2,41 @@ import React from "react";
 import "./LoginStyles.css";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import axios from "axios";
+import { useState, useEffect, useRef } from "react";
 
 function LoginPage() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  function handleClick(events) {
+    events.preventDefault();
+    axios
+      .post(
+        "http://140.119.19.16:8001/login/",
+        {
+          email: emailRef.current.value,
+          password: passwordRef.current.value,
+        },
+        {
+          headers: {
+            "X-CSRFToken":
+              "98AcVKLrlGVptDY7oMFZUFjAJ0Gd2s1gS6g2Ik7v2c5vnIPZQtNBEWFnOt0LGgUK",
+          },
+        }
+      )
+      .then(function (response) {
+        if (response.status === 200)
+          sessionStorage.setItem("key", response.data.key);
+        console.log(response);
+      })
+      .catch(function (error) {
+        if (error.response.status === 400)
+          alert("Please check your email and password");
+        console.log(error);
+      });
+  }
+
   return (
     <section>
       <motion.div
@@ -19,12 +52,12 @@ function LoginPage() {
               <h2>Login</h2>
               <div className="inputbox">
                 <ion-icon name="mail-outline"></ion-icon>
-                <input type="email" required />
+                <input type="email" required ref={emailRef} />
                 <label>Email</label>
               </div>
               <div className="inputbox">
                 <ion-icon name="lock-closed-outline"></ion-icon>
-                <input type="password" required />
+                <input type="password" required ref={passwordRef} />
                 <label>Password</label>
               </div>
               <div className="forget">
@@ -36,7 +69,7 @@ function LoginPage() {
                 </a>
               </div>
 
-              <button>Log in</button>
+              <button onClick={handleClick}>Log in</button>
               <div className="register">
                 <p>
                   Don't have an account? <Link to="/register">Register</Link>
--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -1,33 +1,79 @@
-import React from 'react';
-import "./LoginStyles.css";
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./RegisterStyle.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect, useRef } from "react";
+import { username } from "react-lorem-ipsum";
 
 function RegisterPage() {
-    return(
-        <section>
-            <div className="form-box">
-                <div className="form-value">
-                    <form action="">
-                        <h2>Sign Up</h2>
-                        <div className="inputbox">
-                            <ion-icon name="mail-outline"></ion-icon>
-                            <input type="email" required />
-                            <label>Email</label>
-                        </div>
-                        <div className="inputbox">
-                            <ion-icon name="lock-closed-outline"></ion-icon>
-                            <input type="password" required />
-                            <label>Password</label>
-                        </div>
-                        <button>Register</button>
-                        <div className="register">
-            <p>Already have an account? <Link to="/login">Log In</Link></p>
+  const usernameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const confirmpasswordRef = useRef(null);
+
+  function handleClick(events) {
+    events.preventDefault();
+    if (passwordRef.current.value === confirmpasswordRef.current.value) {
+      axios
+        .post("http://140.119.19.16:8001/accounts/", {
+          username: usernameRef.current.value,
+          email: emailRef.current.value,
+          password1: passwordRef.current.value,
+          password2: confirmpasswordRef.current.value,
+        })
+        .then(function (response) {
+          if (response.status === 204) location.href = "/login";
+          console.log(response);
+        })
+        .catch(function (error) {
+          if (error.response.status === 400 && error.response.data)
+            alert(error.response.data);
+          console.log(error);
+        });
+    } else {
+      alert(
+        "Please notify that password and confirmation password need to be equivalent"
+      );
+    }
+  }
+
+  return (
+    <section>
+      <div className="registerform-box">
+        <div className="form-value">
+          <form action="">
+            <h2>Sign Up</h2>
+            <div className="inputbox">
+              <ion-icon name="mail-outline"></ion-icon>
+              <input type="text" required ref={usernameRef} />
+              <label>Username</label>
             </div>
-                    </form>
-                </div>
+            <div className="inputbox">
+              <ion-icon name="mail-outline"></ion-icon>
+              <input type="email" required ref={emailRef} />
+              <label>Email</label>
             </div>
-        </section>
-    )
+            <div className="inputbox">
+              <ion-icon name="lock-closed-outline"></ion-icon>
+              <input type="password" required ref={passwordRef} />
+              <label>Password</label>
+            </div>
+            <div className="inputbox">
+              <ion-icon name="lock-closed-outline"></ion-icon>
+              <input type="password" required ref={confirmpasswordRef} />
+              <label>Confirm Password</label>
+            </div>
+            <button onClick={handleClick}>Register</button>
+            <div className="register">
+              <p>
+                Already have an account? <Link to="/login">Log In</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default RegisterPage;
